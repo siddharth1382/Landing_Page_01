cac28ada55219f1583ee5088475b0e5817f8e015
siddharth1382 <bailwalsiddharth@gmail.com>
2024-08-29 18:48:33 +0530

    Initial commit of my front-end Project

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+function loading(){
+    var tl = gsap.timeline();
+
+    tl.to("#yellow1" , {
+        top:"-100%",
+        delay:0.5,
+        duration:0.7,
+        ease:"expo.out"
+    })
+    
+    tl.from("#yellow2"  ,
+    {
+        top:"100%",
+        delay:0.6,
+        duration:0.5,
+        ease:"expo.out"
+    } , "anim")
+    
+    tl.to("#loader h1" , {
+        delay:0.6,
+        duration:0.7,
+        color:"black"
+    } , "anim")
+    
+    tl.to("#loader" , {
+        opacity:0,
+        display:"none"
+    })
+    
+}
+
+loading()
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+document.querySelector("#footer h2").addEventListener("click" , ()=>{
+    scroll.scrollTo(0)
+})
+
+var elems = document.querySelectorAll(".elem")
+var page2 = document.querySelector("#page2")
+
+elems.forEach(function(ele){
+    ele.addEventListener("mouseenter" , function(){
+        var bgimg = ele.getAttribute("data-img")
+        page2.style.backgroundImage = `url(${bgimg})`
+    })
+})
